735bb1b11d5cc2a1b01d0b294365dd052e776dd1
Tmk <i@tmk.im>
2023-12-29 12:17:25 +0800

    refactor: simplify the implementation of content-script hmr plugin

--- packages/modernjs-plugin/src/plugins/content-scripts/hmr-plugin.ts
+++ packages/modernjs-plugin/src/plugins/content-scripts/hmr-plugin.ts
@@ -6,28 +6,6 @@ export class ContentScriptHMRPlugin {
   apply(compiler: webpackNS.Compiler) {
     const { contentScriptEntries } = this;
 
-    const {
-      webpack,
-      webpack: { RuntimeGlobals },
-    } = compiler;
-
-    class ContentScriptHMRRuntimeModule extends webpack.RuntimeModule {
-      constructor() {
-        super('content-script hmr runtime', webpack.RuntimeModule.STAGE_ATTACH);
-      }
-
-      generate() {
-        if (!this.chunk) return null;
-        return webpack.Template.asString([
-          // jsonp will cause cross-context issues in the isolated content-script environment
-          `${RuntimeGlobals.loadScript} = async function (url, done, key, chunkId) {
-            await import(url);
-            done(null);
-          };`,
-        ]);
-      }
-    }
-
     compiler.hooks.thisCompilation.tap('ContentScriptHMRPlugin', (compilation) => {
       const isEnabledForChunk = (chunk: webpackNS.Chunk) => {
         const entryName = chunk.getEntryOptions()?.name;
@@ -35,50 +13,26 @@ export class ContentScriptHMRPlugin {
         return contentScriptEntries.has(entryName);
       };
 
-      const onceForChunkSet = new WeakSet<webpackNS.Chunk>();
-      const handler = (chunk: webpackNS.Chunk, set: Set<string>) => {
-        if (onceForChunkSet.has(chunk)) return;
-        onceForChunkSet.add(chunk);
+      compilation.hooks.runtimeModule.tap('ContentScriptHMRPlugin', (module, chunk) => {
         if (!isEnabledForChunk(chunk)) return;
-        set.add(RuntimeGlobals.moduleFactoriesAddOnly);
-        set.add(RuntimeGlobals.hasOwnProperty);
-        compilation.addRuntimeModule(chunk, new ContentScriptHMRRuntimeModule());
-      };
-      compilation.hooks.runtimeRequirementInTree
-        .for(RuntimeGlobals.ensureChunkHandlers)
-        .tap('ContentScriptHMRPlugin', handler);
-      compilation.hooks.runtimeRequirementInTree
-        .for(RuntimeGlobals.hmrDownloadUpdateHandlers)
-        .tap('ContentScriptHMRPlugin', handler);
-      compilation.hooks.runtimeRequirementInTree
-        .for(RuntimeGlobals.hmrDownloadManifest)
-        .tap('ContentScriptHMRPlugin', handler);
-      compilation.hooks.runtimeRequirementInTree.for(RuntimeGlobals.baseURI).tap('ContentScriptHMRPlugin', handler);
-
-      compilation.hooks.runtimeRequirementInTree
-        .for(RuntimeGlobals.ensureChunkHandlers)
-        .tap('ContentScriptHMRPlugin', (chunk, set) => {
-          if (!isEnabledForChunk(chunk)) return;
-          set.add(RuntimeGlobals.publicPath);
-          set.add(RuntimeGlobals.getChunkScriptFilename);
-        });
-      compilation.hooks.runtimeRequirementInTree
-        .for(RuntimeGlobals.hmrDownloadUpdateHandlers)
-        .tap('ContentScriptHMRPlugin', (chunk, set) => {
-          if (!isEnabledForChunk(chunk)) return;
-          set.add(RuntimeGlobals.publicPath);
-          set.add(RuntimeGlobals.getChunkUpdateScriptFilename);
-          set.add(RuntimeGlobals.moduleCache);
-          set.add(RuntimeGlobals.hmrModuleData);
-          set.add(RuntimeGlobals.moduleFactoriesAddOnly);
-        });
-      compilation.hooks.runtimeRequirementInTree
-        .for(RuntimeGlobals.hmrDownloadManifest)
-        .tap('ContentScriptHMRPlugin', (chunk, set) => {
-          if (!isEnabledForChunk(chunk)) return;
-          set.add(RuntimeGlobals.publicPath);
-          set.add(RuntimeGlobals.getUpdateManifestFilename);
-        });
+        if (module.name === 'load script') patchLoadScriptRuntimeModule(module, compiler);
+      });
     });
   }
+}
+
+function patchLoadScriptRuntimeModule(
+  module: webpackNS.RuntimeModule,
+  { webpack, webpack: { RuntimeGlobals } }: webpackNS.Compiler
+) {
+  module.name = 'content-script load script';
+  module.generate = function (this: webpackNS.RuntimeModule) {
+    return webpack.Template.asString([
+      // jsonp will cause cross-context issues in the isolated content-script environment
+      `${RuntimeGlobals.loadScript} = async function (url, done, key, chunkId) {
+        await import(url);
+        done(null);
+      };`,
+    ]);
+  };
 }
