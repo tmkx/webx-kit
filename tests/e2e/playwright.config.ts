42dab8980d22b0cc189f0c0e0440cc677a41c7b1
Tmk <i@tmk.im>
2023-12-30 15:02:31 +0800

    feat: add e2e test

---
+++ tests/e2e/playwright.config.ts
@@ -0,0 +1,26 @@
+/**
+ * @see {@link https://playwright.dev/docs/chrome-extensions Chrome extensions | Playwright}
+ */
+import { defineConfig } from '@playwright/test';
+import { createLaunchOptions } from './tests/webx-test';
+
+export default defineConfig({
+  testDir: './tests',
+  retries: 2,
+  projects: [
+    {
+      name: 'React',
+      grep: /react/,
+      use: {
+        launchOptions: createLaunchOptions('@webx-kit/example-react'),
+      },
+    },
+    {
+      name: 'Vue',
+      grep: /vue/,
+      use: {
+        launchOptions: createLaunchOptions('@webx-kit/example-vue'),
+      },
+    },
+  ],
+});
