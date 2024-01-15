c9fb3594fa3bb010a0099eea4f74a9a2ec320b50
Tmk <i@tmk.im>
2024-01-15 13:44:53 +0800

    feat: display translate result in ui

--- apps/ai-assistant/src/content-scripts/app.tsx
+++ apps/ai-assistant/src/content-scripts/app.tsx
@@ -1,12 +1,18 @@
-import { useEffect, useState } from 'react';
-import { Button, ConfigProvider } from '@douyinfe/semi-ui';
+import { useEffect, useMemo, useState } from 'react';
+import { Button, Card, ConfigProvider, Spin } from '@douyinfe/semi-ui';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import clsx from 'clsx';
 import './global.less';
 
+// hack for missing button loading rotate keyframes
+Spin.name;
+
 export const App = () => {
   const [visible, setVisible] = useState(false);
   const [rootStyle, setRootStyle] = useState<React.CSSProperties>({ left: 0, top: 0 });
+  const [isLoading, setIsLoading] = useState(false);
+  const [content, setContent] = useState('');
+  const isDarkMode = useMemo(isPageInDark, [visible]);
 
   useEffect(() => {
     const ac = new AbortController();
@@ -26,12 +32,53 @@ export const App = () => {
     return () => ac.abort();
   }, []);
 
+  useEffect(() => {
+    if (!visible) setContent('');
+  }, [visible]);
+
+  const handleTranslate = async (text: string) => {
+    if (!genAI) {
+      return console.warn('skipped calling gemini-pro');
+    }
+
+    setContent('');
+    setIsLoading(true);
+    try {
+      const result = await genAI.getGenerativeModel({ model: 'gemini-pro' }).generateContentStream({
+        contents: [
+          {
+            role: 'user',
+            parts: [{ text: 'Translate the following text to Chinese:\n' + text }],
+          },
+        ],
+      });
+      for await (const token of result.stream || []) {
+        setContent((prev) => prev + token.text());
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <ConfigProvider getPopupContainer={() => window.__webxRoot as unknown as HTMLElement}>
-      <div className={clsx('absolute', visible ? 'block' : 'hidden')} style={rootStyle}>
-        <Button theme="solid" type="primary" onClick={logSelectedText}>
-          Log selected Text
+      <div
+        className={clsx('absolute', visible ? 'block' : 'hidden', isDarkMode ? 'semi-always-dark' : null)}
+        style={rootStyle}
+      >
+        <Button
+          theme="solid"
+          type="primary"
+          loading={isLoading}
+          onClick={() => {
+            const selectedText = getSelectedText();
+            if (!selectedText) return;
+            handleTranslate(selectedText);
+          }}
+        >
+          Translate
         </Button>
+        {!content ? null : <Card className="min-w-96">{content}</Card>}
       </div>
     </ConfigProvider>
   );
@@ -48,7 +95,7 @@ chrome.storage.local.get(GOOGLE_API_KEY).then(({ GOOGLE_API_KEY }) => {
   genAI = new GoogleGenerativeAI(GOOGLE_API_KEY);
 });
 
-async function logSelectedText() {
+function getSelectedText() {
   const selection = getSelection();
   if (!selection || selection.isCollapsed) return;
   const { rangeCount } = selection;
@@ -57,20 +104,13 @@ async function logSelectedText() {
     text += selection.getRangeAt(i).cloneContents().textContent;
   }
   console.log('Selected text:', text);
+  return text;
+}
 
-  if (!genAI) {
-    return console.warn('skipped calling gemini-pro');
-  }
-
-  const result = await genAI.getGenerativeModel({ model: 'gemini-pro' }).generateContentStream({
-    contents: [
-      {
-        role: 'user',
-        parts: [{ text: 'Translate the following text to Chinese:\n' + text }],
-      },
-    ],
-  });
-  for await (const token of result.stream || []) {
-    console.log('RECEIVE', token.text());
-  }
+function isPageInDark() {
+  const { colorScheme, backgroundColor } = getComputedStyle(document.body);
+  if (colorScheme === 'dark') return true;
+  const bgColor = backgroundColor.match(/rgba?\((\d+), (\d+), (\d+)/);
+  if (bgColor) return Number(bgColor[1]) + Number(bgColor[2]) + Number(bgColor[3]) < (256 / 2) * 3;
+  return false;
 }
