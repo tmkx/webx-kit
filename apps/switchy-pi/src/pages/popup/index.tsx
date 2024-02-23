6ddf27d7d312f843ae453e7c4ea1ebe2da49a6d9
Tmk <i@tmk.im>
2024-02-23 15:00:01 +0800

    feat: implement switchy-pi popup

--- apps/switchy-pi/src/pages/popup/index.tsx
+++ apps/switchy-pi/src/pages/popup/index.tsx
@@ -2,4 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { App } from './app';
 import '../global.less';
 
+if (matchMedia('(prefers-color-scheme: dark)').matches) {
+  document.body.classList.add('dark');
+}
 createRoot(document.getElementById('root')!).render(<App />);
