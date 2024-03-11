ed3a5f83430c170659e8647b8e0ebeba38f58664
Tmk <i@tmk.im>
2024-03-12 01:48:59 +0800

    feat: support export pac file

---
+++ apps/switchy-pi/src/utils/misc.ts
@@ -0,0 +1,10 @@
+export function downloadTextAsFile(content: string, filename: string) {
+  const blob = new Blob([content], { type: 'text/plain;charset=utf-8' });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.href = url;
+  link.download = filename;
+  link.target = '_blank';
+  link.click();
+  URL.revokeObjectURL(url);
+}
---
+++ apps/switchy-pi/src/utils/pac.ts
@@ -0,0 +1,55 @@
+import type { FixedProfile, ProxyServer } from '@/schemas';
+
+export function generatePACFromFixedProfile(server: FixedProfile) {
+  return `
+var FindProxyForURL = (function (init, profiles) {
+  return function (url, host) {
+    'use strict';
+    var result = init,
+      scheme = url.substr(0, url.indexOf(':'));
+    do {
+      result = profiles[result];
+      if (typeof result === 'function') result = result(url, host, scheme);
+    } while (typeof result !== 'string' || result.charCodeAt(0) === 43);
+    return result;
+  };
+})(${JSON.stringify(server.name)}, {
+  ${JSON.stringify(server.name)}: ${generateFunction(server)},
+  });
+  `;
+}
+
+function generateFunction(server: FixedProfile) {
+  return `function (url, host, scheme) {
+    'use strict';
+    ${
+      server.bypassList.length > 0
+        ? `if (${server.bypassList
+            .map((item) => `/^${item.pattern.replace(/\./g, '\\.')}$/.test(host)`)
+            .join(' || ')}) return 'DIRECT';`
+        : ''
+    }
+    switch (scheme) {
+      ${server.proxyForHttp ? `case 'http': return ${buildProxyServerString(server.proxyForHttp)};` : ''}
+      ${server.proxyForHttps ? `case 'https': return ${buildProxyServerString(server.proxyForHttps)};` : ''}
+      ${server.proxyForFtp ? `case 'ftp': return ${buildProxyServerString(server.proxyForFtp)};` : ''}   
+      default:
+        return ${buildProxyServerString(server.fallbackProxy)};
+    }
+  }`;
+}
+
+function buildProxyServerString(server: ProxyServer, withQuote = true) {
+  const hostPort = `${server.host}:${server.port}`;
+  const wrap = (str: string) => (withQuote ? `"${str}"` : str);
+  switch (server.scheme) {
+    case 'http':
+      return wrap(`PROXY ${hostPort}`);
+    case 'https':
+      return wrap(`HTTPS ${hostPort}`);
+    case 'socks4':
+      return wrap(`SOCKS ${hostPort}`);
+    case 'socks5':
+      return wrap(`SOCKS5 ${hostPort}; SOCKS ${hostPort}`);
+  }
+}
