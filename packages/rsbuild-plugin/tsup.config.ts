b854e5fcca4a8e7a4c37b6aa1b7b7609cfd79a65
Tmk <i@tmk.im>
2024-03-16 22:00:17 +0800

    fix: add missing rsbuild plugin entry

--- packages/rsbuild-plugin/tsup.config.ts
+++ packages/rsbuild-plugin/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: {
     index: './src/index.ts',
+    manifest: './src/manifest.ts',
   },
   outDir: './dist',
   format: 'cjs',
